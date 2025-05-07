1ae925267a2895c498496c783efa3e3b0d345861
NicoMasss <nickmassochin@gmail.com>
2025-05-06 23:54:45 -0300

    First Commit

---
+++ ProductClientHub/Controllers/Controller.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductClientHub.Controllers
+{
+    [Route("api/Clientes")]
+    [ApiController]
+
+    public class Controller : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
